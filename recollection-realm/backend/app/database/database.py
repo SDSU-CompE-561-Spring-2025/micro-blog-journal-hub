21941f14723752b5a79e994bbcffbb773bed723d
RhiloSotto <rnsotto@gmail.com>
2025-04-13 16:53:46 -0700

    renamed database.py and database connection added

--- recollection-realm/backend/app/database/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker #maybe more
-#other imports
-
-engine = create_engine() #<-- fill in
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-#other stuff
