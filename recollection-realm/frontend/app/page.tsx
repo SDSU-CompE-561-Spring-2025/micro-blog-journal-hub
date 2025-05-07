fb65d3a88225024ba827a187eddf26d6fa2f0a26
RhiloSotto <rnsotto@gmail.com>
2025-05-06 21:04:38 -0700

    supabase db files added from Next.js app frameworks connection

--- recollection-realm/frontend/app/page.tsx
+++ recollection-realm/frontend/app/page.tsx
@@ -1,4 +1,25 @@
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+
+
 import { LoginForm } from "@/components/login-form"
+
+
+
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('todos').select()
+
+  return (
+    <ul>
+      {todos?.map((todo) => (
+        <li>{todo}</li>
+      ))}
+    </ul>
+  )
+}
 
 export default function Home() {
   return (
